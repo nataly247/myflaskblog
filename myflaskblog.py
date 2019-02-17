5f924a6d88814c49d4ed38dd6549531ed95af35a
nataly247 <nataly247@gmail.com>
2019-02-17 10:07:29 +0200

    added list of posts

--- myflaskblog.py
+++ myflaskblog.py
@@ -3,10 +3,27 @@ from flask import render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Nataly Pan',
+        'date_posted': 'Fabruary 17, 2019',
+        'title': 'Blog Post 1',
+        'content': 'First post content'
+    },
+    {
+        'author': 'James Bond',
+        'date_posted': 'Fabruary 18, 2019',
+        'title': 'Blog Post 2',
+        'content': 'Second post content'
+    }
+]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
 
 @app.route('/about')
 def about():
