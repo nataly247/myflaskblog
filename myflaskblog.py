4a9b03aac9e63ef68807d2615b2ae9bfec9c1f5f
nataly247 <nataly247@gmail.com>
2019-02-17 22:44:30 +0200

    added register function

--- myflaskblog.py
+++ myflaskblog.py
@@ -1,30 +1,33 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Nataly Pan',
-        'date_posted': 'Fabruary 17, 2019',
-        'title': 'Blog Post 1',
-        'content': 'First post content'
-    },
-    {
-        'author': 'James Bond',
-        'date_posted': 'Fabruary 18, 2019',
-        'title': 'Blog Post 2',
-        'content': 'Second post content'
-    }
-]
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', title='Articles')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
