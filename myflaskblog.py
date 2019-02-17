919e67a115f561529be2884407a7fb7394057900
nataly247 <nataly247@gmail.com>
2019-02-17 12:46:26 +0200

    looping through posts

--- myflaskblog.py
+++ myflaskblog.py
@@ -22,7 +22,7 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
